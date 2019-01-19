920a74513b528165d709c5b2b8f50d6407f58908
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-01-18 23:03:35 -0500

    initial set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Angus Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
